9d66ee46c459630a4ea32fe8778a46a58815db0e
Radicalchaos <30750791+Radicalchaos@users.noreply.github.com>
2017-10-06 15:47:54 +0200

    save all changes

--- scripts/main.js
+++ scripts/main.js
@@ -13,13 +13,13 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'healthy life!, ' + myName;
+  myHeading.textContent = 'Niku lifestyle, ' + myName;
 }
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'healthy life!, ' + storedName;
+  myHeading.textContent = 'Niku lifestyle, ' + storedName;
 }
 myButton.onclick = function() {
   setUserName();
